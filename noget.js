738a5cf59cd6a0d58834bef0304f277710dd50cb
andrewgwallace <andrew.g.wallace@accenturefederal.com>
2018-03-05 23:02:48 -0500

    add definitions to dictionary

--- noget.js
+++ noget.js
@@ -1,4 +1,4 @@
-define(['supreme'], function(supreme) {
+define(['legend'], function(supreme) {
 
     var translate = function(sourceText) {
         sourceText = sourceText.replace(/\W/g, '');
@@ -30,6 +30,59 @@ define(['supreme'], function(supreme) {
         }
         else {
             sourceText = sourceText.replace(/\W/g, '');
+            var supremeAlphanumerics = supreme.scroll.alphanumerics;
+            var firstChar = sourceText.split('')[0].toUpperCase();
+            var restChars = sourceText.substring(1, sourceText.length);
+            return supremeAlphanumerics.word[firstChar] + ' ' + recurse(restChars);
+        }
+    }
+
+    function sumArray(numbers) {
+        var sum = 0;
+        for (var i = 0; i < numbers.length; i++) {
+            sum += parseInt(numbers[i]);
+          }
+          return sum;
+    }
+
+    return { translate: translate }
+});
+
+
+/*
+
+define(['supreme'], function(supreme) {
+
+    var translate = function(sourceText) {
+        sourceText = sourceText.replace(/\W/g, '');
+
+        var translation = '';
+        translation += recurse(sourceText);
+
+        while(sourceText.length > 1) {
+            var numbers = sourceText.split('');
+            var sum = sumArray(numbers);
+
+            if(numbers[0] > numbers[1] && numbers[1] > 0) {
+                translation += 'not being born to ';
+            }
+            else {
+                translation += 'all being born to ';
+            }
+
+            sourceText = sum.toString();
+            translation += recurse(sourceText);
+        }
+
+        return translation;
+    }
+
+    function recurse(sourceText) {
+        if(sourceText.length == 0) {
+            return '';
+        }
+        else {
+            sourceText = sourceText.replace(/\W/g, '');
             var supremeAlphanumerics = supreme.alphanumerics;
             var firstChar = sourceText.split('')[0].toUpperCase();
             var restChars = sourceText.substring(1, sourceText.length);
@@ -47,3 +100,5 @@ define(['supreme'], function(supreme) {
 
     return { translate: translate }
 });
+
+ */
